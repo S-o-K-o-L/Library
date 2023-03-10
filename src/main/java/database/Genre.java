df86e984bcf571274142a8a20c8068a3a2f2942a
rozhkov <rozhkov-aleksander.rozhkov@yandex.ru>
2023-03-10 11:24:13 +0300

    some changes

---
+++ src/main/java/database/Genre.java
@@ -0,0 +1,22 @@
+package database;
+
+public class Genre {
+    private Integer idGenre;
+    private String nameGenre;
+
+    public void setIdGenre(Integer idGenre) {
+        this.idGenre = idGenre;
+    }
+
+    public void setNameGenre(String nameGenre) {
+        this.nameGenre = nameGenre;
+    }
+
+    public Integer getIdGenre() {
+        return idGenre;
+    }
+
+    public String getNameGenre() {
+        return nameGenre;
+    }
+}
--- src/main/java/database/Main.java
+++ src/main/java/database/Main.java
@@ -3,17 +3,28 @@ package database;
 import java.sql.*;
 
 public class Main {
+
+    //вынести URL в properties
+    //сделать инт лонгом -  в бд Бигнитом
+    //entity и др сущности
+    //genre сделать enum
+    //поставить default значения для NOT NULL
+
     private static final String URL = "jdbc:postgresql://localhost:5432/Library";
     private static final String user = "postgres";
-    private static final String password = "1";
+    private static final String password = "1111";
 
     public static void main(String[] args) {
-        try(Connection connection = DriverManager.getConnection(URL,user,password);
-            Statement statement = connection.createStatement()
+        try (Connection connection = DriverManager.getConnection(URL, user, password);
+             Statement statement = connection.createStatement()
         ) {
-            ResultSet resultSet = statement.executeQuery("select * from users");
-            resultSet.next();
-            System.out.println(resultSet.getString(4));
+            ResultSet resultSet = statement.executeQuery("select * from genre");
+            Genre genre = new Genre();
+            while (resultSet.next()) {
+                genre.setIdGenre(resultSet.getInt(1));
+                genre.setNameGenre(resultSet.getString(2));
+            }
+            System.out.println(genre.getIdGenre() + " - " + genre.getNameGenre());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
